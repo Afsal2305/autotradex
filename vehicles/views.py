8311244a50d89b405c70db0b080d6f052805107b
Afsal2305 <Afsalofficial2024@gmail.com>
2025-07-20 00:14:01 +0530

    Completed Phase 3 - Sell & Buy Vehicles Functional

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,27 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Vehicle
 from django.contrib.auth.decorators import login_required
-
+from .forms import VehicleForm
 @login_required
+
+
 def sell_vehicle(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        brand = request.POST['brand']
-        price = request.POST['price']
-        year = request.POST['year']
-        description = request.POST['description']
-        image = request.FILES['image']
-        Vehicle.objects.create(
-            seller=request.user,
-            title=title,
-            brand=brand,
-            price=price,
-            year=year,
-            description=description,
-            image=image
-        )
-        return redirect('buy_vehicle')
-    return render(request, 'vehicles/sell.html')
+        form = VehicleForm(request.POST, request.FILES)
+        if form.is_valid():
+            vehicle = form.save(commit=False)
+            vehicle.seller = request.user
+            vehicle.save()
+            return redirect('buy_vehicle')
+    else:
+        form = VehicleForm()
+    return render(request, 'vehicles/sell.html', {'form': form})
+
 
 def buy_vehicle(request):
     vehicles = Vehicle.objects.all().order_by('-created_at')
